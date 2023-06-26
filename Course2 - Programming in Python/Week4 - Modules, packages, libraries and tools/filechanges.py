93d73b81735fa9b607ac095f55f140a44c60aeb8
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-06-26 15:02:54 -0700

    finished w4 assignment 1

---
+++ Course2 - Programming in Python/Week4 - Modules, packages, libraries and tools/filechanges.py
@@ -0,0 +1,6 @@
+import os
+
+def print_changes():
+    contents = os.listdir(r'C:\Users\ycho8\Desktop\Coding\Coursera\Meta-Back-End-Developer\Course2 - Programming in Python\Week4 - Modules, packages, libraries and tools')
+    print("Current directory contents: ")
+    print(contents)
