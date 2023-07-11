f6f4dba420a75001b2e8302733c42469a8683861
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-07-10 21:50:46 -0700

    finished week 2

--- Course5 - Django Web Framework/Week2 - Views/notes.py
+++ Course5 - Django Web Framework/Week2 - Views/notes.py
@@ -42,5 +42,34 @@ Use the url tag to obtain the URL path dynamically, as shown below:
 
 ** the login view may be present in multiple apps. Use the named URL qualified with the namespace to resolve the conflict.
 <form action="{% url 'demoapp:login' %}" method="post">
+'''
+
+# in Django application, a view function is where all the processing is done. it receives the request and formulates the response.
+from django.http import HttpResponse, HttpResponseNotFound
+
+def my_view(request):
+    # code
+    if condition == True:
+        return HttpResponse('<h1>Page not found</h1>', status_code='404')
+        # OR
+        # return HttpResponseNotFound('<h1>Page not found</h1>')
+    else:
+        return HttpResponse('<h1>Page was found</h1>')
 
 '''
+Consider the following scenario, where you have a Product model in the app.
+The user wants the details of a product with a specific Product ID.
+In the following view function, id is the parameter obtained from the URL.
+It tries to determine whether any product with the given id is available. If not, the Http404 exception is raised.
+'''
+from django.http import Http404, HttpResponse
+from .models import Product
+
+def detail(request, id):
+    try:
+        p = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        raise Http404("Product does not exist")
+    return HttpResponse("Product Found")
+
+# To display the custom error message in the browser, change DEBUG = False in settings.py
